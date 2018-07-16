6c111beb8840628a92e4803503e489246cbd257e
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-16 21:01:37 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,10 +4,10 @@ class PigLatinizer
 
 
   def piglatinize(word)
-    match = word.match(/[aeiou]/)
-   return word unless match
-   pre = match.pre_match
-   post = match.post_match
-   return match.to_s + post + pre + "ay"
- end
+      match = word.match(/[aeiou]/)
+     return word unless match
+     pre = match.pre_match
+     post = match.post_match
+     return match.to_s + post + pre + "ay"
+   end
 end

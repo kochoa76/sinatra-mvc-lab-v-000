c1844808967a70570aca4f32764ccc33fcd428a7
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-16 21:01:27 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,4 +2,12 @@ class PigLatinizer
 
   attr_accessor :words
 
+
+  def piglatinize(word)
+    match = word.match(/[aeiou]/)
+   return word unless match
+   pre = match.pre_match
+   post = match.post_match
+   return match.to_s + post + pre + "ay"
+ end
 end

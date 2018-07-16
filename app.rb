57666fdc1faa1c20ca45bc0434501e6de8098e10
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-16 21:28:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
     erb :user_input
   end
 
-  post "/piglatinize" do
+  post "/results" do
     pig = PigLatinizer.new
     @piglatinizer= pig.piglatinize([params:user_phrase])
 
